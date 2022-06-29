6f0fc434cd3ca4d35c0c113d7b5b8a1908cf8e89
Jemery Day <jemeryd@spc.int>
2022-06-29 23:58:57 +0000

    Add repair_penguin.R

---
+++ R/stepwise/repair_penguin.R
@@ -0,0 +1,5 @@
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/_gsdata_",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+
