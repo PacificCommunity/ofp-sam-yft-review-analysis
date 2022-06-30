e77e5573642e17774b621357a74489f6dd92ecba
Jemery Day <jemeryd@spc.int>
2022-06-30 01:24:26 +0000

    First set of files added from Matt's backup

--- R/stepwise/repair_penguin.R
+++ R/stepwise/repair_penguin.R
@@ -1,5 +1,41 @@
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/_gsdata_",
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step8NoEff",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)  
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step9aaNoAge0Ungroup",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step12JPTP",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step14LW",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.rename("Z:/yft/2020/assessment/ModelRuns/Stepwise/Step14LW",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise/Step14LW_HopefulReport")
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step14LW",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step15CondAgeLen",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step16MatLength",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step17NoSpawnFrac",
+          "Z:/yft/2020/assessment/ModelRuns/Stepwise",
+          recursive = TRUE,
+          copy.date = TRUE)
+file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/AltDiags/CondVBSize60",
           "Z:/yft/2020/assessment/ModelRuns/Stepwise",
           recursive = TRUE,
           copy.date = TRUE)
 
+file.remove("Z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/DiagnosticPlots") # Permission denied!
+file.remove("Z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/Figures") # Permission denied!
