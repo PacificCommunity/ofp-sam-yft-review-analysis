c604d7437216b07fd07d69af08aaf589293594dd
Jemery Day <jemeryd@spc.int>
2022-07-01 00:58:15 +0000

    define file paths at top of script

--- R/stepwise/repair_penguin.R
+++ R/stepwise/repair_penguin.R
@@ -1,46 +1,53 @@
-## 09_IdxNoeff
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step8NoEff",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+path.hopeful <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful"
+path.selstep <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep"
+path.altdiags <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/AltDiags"
+path.diagnostic <- "z:/yft/2020/assessment/ModelRuns/Diagnostic"
+path.destination <- "z:/yft/2020_review/analysis/stepwise"
 
+## 09_IdxNoeff
+file.copy(file.path(path.hopeful, "Step8NoEff"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
+file.rename(file.path(path.destination, "Step14LW"),
+            file.path(path.destination, "Step14LW_HopefulReport"))
 ## 10_SelUngroup
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step9aaNoAge0Ungroup",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.hopeful, "Step9aaNoAge0Ungroup"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 11_JPTP
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step12JPTP",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.selstep, "Step12JPTP"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 12_Age10LW_HopefulReport
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful/Step14LW",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
-file.rename("z:/yft/2020/assessment/ModelRuns/Stepwise/Step14LW",
-            "z:/yft/2020/assessment/ModelRuns/Stepwise/Step14LW_HopefulReport")
+file.copy(file.path(path.hopeful, "Step14LW"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
+file.rename(file.path(path.destination, "Step14LW"),
+            file.path(path.destination, "Step14LW_HopefulReport"))
 
 ## 12_Age10LW
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step14LW",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.selstep, "Step14LW"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 13_CondAge
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step15CondAgeLen",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.selstep, "Step15CondAgeLen"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 14_MatLength
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step16MatLength",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.selstep, "Step16MatLength"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 15_NoSpnFrac
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep/Step17NoSpawnFrac",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.selstep, "Step17NoSpawnFrac"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## 16_Size60
-file.copy("d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/AltDiags/CondVBSize60",
-          "z:/yft/2020/assessment/ModelRuns/Stepwise", recursive=TRUE, copy.date=TRUE)
+file.copy(file.path(path.altdiags, "CondVBSize60"),
+          path.destination, recursive=TRUE, copy.date=TRUE)
 
 ## Remove subdirs
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/DiagnosticPlots", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/Figures", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/Hessian", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/Impact", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/JohnLProf", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/Kobe", recursive=TRUE)
-unlink("z:/yft/2020/assessment/ModelRuns/Stepwise/CondVBSize60/SavePhase11NoMChange", recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/DiagnosticPlots"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/Figures"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/Hessian"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/Impact"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/JohnLProf"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/Kobe"), recursive=TRUE)
+unlink(file.path(path.destination, "CondVBSize60/SavePhase11NoMChange"), recursive=TRUE)
