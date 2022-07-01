0854d14f7cb4970093629ae3a905e6c5672affff
Jemery Day <jemeryd@spc.int>
2022-07-01 02:28:57 +0000

    Complete script to copy directories

--- R/stepwise/repair_penguin.R
+++ R/stepwise/repair_penguin.R
@@ -1,3 +1,5 @@
+library(TAF)
+
 path.hopeful <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/Hopeful"
 path.selstep <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/SelStep"
 path.altdiags <- "d:/Vincent_Matthew_Backup/YFT/2020/assessment/ModelRuns/AltDiags"
@@ -5,53 +7,47 @@ path.diagnostic <- "z:/yft/2020/assessment/ModelRuns/Diagnostic"
 path.destination <- "z:/yft/2020_review/analysis/stepwise"
 
 ## 09_IdxNoeff
-file.copy(file.path(path.hopeful, "Step8NoEff"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.hopeful, "Step8NoEff"), path.destination)
 file.rename(file.path(path.destination, "Step8NoEff"), file.path(path.destination, "09_IdxNoeff"))
 
 ## 10_SelUngroup
-file.copy(file.path(path.hopeful, "Step9aaNoAge0Ungroup"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.hopeful, "Step9aaNoAge0Ungroup"), path.destination)
 file.rename(file.path(path.destination, "Step9aaNoAge0Ungroup"), file.path(path.destination, "10_SelUngroup"))
 
 ## 11_JPTP
-file.copy(file.path(path.selstep, "Step12JPTP"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.selstep, "Step12JPTP"), path.destination)
 file.rename(file.path(path.destination, "Step12JPTP"), file.path(path.destination, "11_JPTP"))
 
 ## 12_Age10LW_HopefulReport
-file.copy(file.path(path.hopeful, "Step14LW"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.hopeful, "Step14LW"), path.destination)
 file.rename(file.path(path.destination, "Step14LW"), file.path(path.destination, "12_Age10LW_HopefulReport"))
 
 ## 12_Age10LW
-file.copy(file.path(path.selstep, "Step14LW"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.selstep, "Step14LW"), path.destination)
 file.rename(file.path(path.destination, "Step14LW"), file.path(path.destination, "12_Age10LW"))
 
 ## 13_CondAge
-file.copy(file.path(path.selstep, "Step15CondAgeLen"), path.destination, recursive=TRUE, copy.date=TRUE)
-file.rename(file.path(path.destination, "Step14LW"), file.path(path.destination, "12_Age10LW"))
+cp(file.path(path.selstep, "Step15CondAgeLen"), path.destination)
+file.rename(file.path(path.destination, "Step15CondAgeLen"), file.path(path.destination, "13_CondAge"))
 
 ## 14_MatLength
-file.copy(file.path(path.selstep, "Step16MatLength"), path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.selstep, "Step16MatLength"), path.destination)
 file.rename(file.path(path.destination, "Step16MatLength"), file.path(path.destination, "14_MatLength"))
 
 ## 15_NoSpnFrac
-file.copy(file.path(path.selstep, "Step17NoSpawnFrac"),path.destination, recursive=TRUE, copy.date=TRUE)
+cp(file.path(path.selstep, "Step17NoSpawnFrac"),path.destination)
 file.rename(file.path(path.destination, "Step17NoSpawnFrac"), file.path(path.destination, "15_NoSpnFrac"))
 
 ## 16_Size60
-# file.copy(file.path(path.altdiags, "CondVBSize60"), path.destination, recursive=TRUE, copy.date=TRUE)
+# cp(file.path(path.altdiags, "CondVBSize60"), path.destination)
 # file.rename(file.path(path.destination, "CondVBSize60"), file.path(path.destination, "16_Size60"))
 dir.create(file.path(path.destination, "16_Size60"))
-file.copy(file.path(path.altdiags, "CondVBSize60/*"), file.path(path.destination, "16_Size60"), copy.date=TRUE)
+files <- dir(file.path(path.altdiags, "CondVBSize60"), full.names=TRUE)
+files <- files[!dir.exists(files)]
+cp(files, file.path(path.destination, "16_Size60"))
 
 ## 17_Diag20
 dir.create(file.path(path.destination, "17_Diag20"))
-file.copy(file.path(path.diagnostic, "/*"), file.path(path.destination, "17_Diag20"), copy.date=TRUE)
-
-
-## Remove subdirs
-#unlink(file.path(path.destination, "16_Size60/DiagnosticPlots"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/Figures"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/Hessian"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/Impact"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/JohnLProf"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/Kobe"), recursive=TRUE)
-#unlink(file.path(path.destination, "16_Size60/SavePhase11NoMChange"), recursive=TRUE)
+files <- dir(path.diagnostic, full.names=TRUE)
+files <- files[!dir.exists(files)]
+cp(files, file.path(path.destination, "17_Diag20"))
