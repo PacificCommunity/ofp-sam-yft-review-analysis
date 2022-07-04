9ef65fc3eea6698a6c971eaa51fa879189adb824
Arni Magnusson <thisisarni@gmail.com>
2022-07-04 14:10:48 +1100

    Use same plot style as report

--- R/stepwise/05_replicate_fig14b_matt.R
+++ R/stepwise/05_replicate_fig14b_matt.R
@@ -7,7 +7,6 @@ source("utilities.R")  # read.plot.rep, reverse
 mkdir("pdf")
 
 penguin <- "z:/yft/2020_review/analysis/stepwise"
-Diag20 <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-14.par.rep")
 
 IdxNoEff <- read.plot.rep(file.path(penguin, "09_IdxNoeff"))
 SelUngroup <- read.MFCLRep(file.path(penguin, "10_SelUngroup/plot-12.par.rep"))
@@ -19,7 +18,6 @@ NoSpnFrac <- read.plot.rep(file.path(penguin, "15_NoSpnFrac"))
 Size60 <- read.plot.rep(file.path(penguin, "16_Size60"))
 Diag20 <- read.MFCLRep(file.path(penguin, "17_Diag20/plot-14.par.rep"))
 
-
 stepwise <- list(IdxNoEff=IdxNoEff,
                 SelUngroup=SelUngroup,
                 JPTP=JPTP,
@@ -30,9 +28,8 @@ stepwise <- list(IdxNoEff=IdxNoEff,
                 Size60=Size60,
                 Diag20=Diag20)
 
-mkdir("pdf")
-pdf("pdf/replicate_fig14b_matt.pdf")
+pdf("pdf/replicate_fig14b_matt.pdf", width=9.5, height=9.5)
 plot.depletion(stepwise, palette.func=reverse) +
-  scale_y_continuous(breaks = seq(0, 1, 0.1)) +
-  theme(panel.grid.major.y=element_line(color="gray", size=0.5))
+  theme(panel.grid.major.y=element_line(color="gray", size=0.5),
+        panel.grid.minor.y=element_line(color="gray", size=0.5))
 dev.off()
