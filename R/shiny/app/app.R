8441a3b7a6cbdeea736559ad7aae5edf2c708cd5
Arni Magnusson <thisisarni@gmail.com>
2022-09-01 06:39:12 +1000

    Update description of app and SPC members

--- R/shiny/app/app.R
+++ R/shiny/app/app.R
@@ -1,13 +1,6 @@
-# Untitled skipjack 2022 stepwise assessement app
+# General Shiny app to compare individual (e.g., stepwise) models
 # 21/04/2022
 # Finlay Scott
-
-#---------------------------------------------------------------------------
-# Notes
-
-# V 0.0.1 The cosmic barrilete
-
-#---------------------------------------------------------------------------
 
 # CRAN packages
 library(shiny)
@@ -28,10 +21,10 @@ library("chorddiag")
 spc_about <- function(){
   out <- tags$html(
     tags$p(style="opacity: 0.5", class="caption", align="center", HTML("&copy"), "Pacific Community, 2022"),
-    tags$p(align="justify", "The Pacific Community (SPC) is the principal scientific and technical organisation in the Pacific region, proudly supporting development since 1947. It is an international development organisation owned and governed by its 26 country and territory members. The members are: American Samoa, Australia, Cook Islands, Federated States of Micronesia, Fiji, France, French Polynesia, Guam, Kiribati, Marshall Islands, Nauru, New Caledonia, New Zealand, Niue, Northern Mariana Islands, Palau, Papua New Guinea, Pitcairn Islands, Samoa, Solomon Islands, Tokelau, Tonga, Tuvalu, United States of America, Vanuatu, and Wallis and Futuna."),
+    tags$p(align="justify", "The Pacific Community (SPC) is the principal scientific and technical organisation in the Pacific region, proudly supporting development since 1947. It is an international development organisation owned and governed by its 27 country and territory members. The members are: American Samoa, Australia, Cook Islands, Federated States of Micronesia, Fiji, France, French Polynesia, Guam, Kiribati, Marshall Islands, Nauru, New Caledonia, New Zealand, Niue, Northern Mariana Islands, Palau, Papua New Guinea, Pitcairn Islands, Samoa, Solomon Islands, Tokelau, Tonga, Tuvalu, United Kingdom, United States of America, Vanuatu, and Wallis and Futuna."),
     tags$p(align="justify", "In pursuit of sustainable development to benefit Pacific people, this unique organisation works across more than 25 sectors. SPC is renowned for its knowledge and innovation in such areas as fisheries science, public health surveillance, geoscience and conservation of plant genetic resources for food and agriculture."),
     tags$p(align="justify", "Much of SPC's focus is on major cross-cutting issues, such as climate change, disaster risk management, food security, gender equality, human rights, non-communicable diseases and youth employment. Using a multi-sector approach in responding to its members' development priorities, SPC draws on skills and capabilities from around the region and internationally, and supports the empowerment of Pacific communities and sharing of expertise and skills between countries and territories."),
-    tags$p(align="justify", "With over 600 staff, SPC has its headquarters in Noumea, regional offices in Suva and Pohnpei, a country office in Honiara and field staff in other Pacific locations. Its working languages are English and French. See: ", a("https://www.spc.int", href="www.spc.int"))
+    tags$p(align="justify", "With over 600 staff, SPC has its headquarters in Noumea, regional offices in Suva and Pohnpei, a country office in Honiara and field staff in other Pacific locations. Its working languages are English and French. See: ", a("https://www.spc.int", href="https://www.spc.int"))
   )
   return(out)
 }
@@ -87,13 +80,9 @@ fishgrp_names <- unique(fishery_map$group)
 
 
 ui <- dashboardPage(
-  header=dashboardHeader(title = "SKJ stepwise 2022"),
-  # Glaucus was a mortal in Greek mythology, who became immortal by eating a magical herb and turned into a prophetic god of the sea. It is uncertain who his parents were.
-  # Source: https://www.greekmythology.com/Myths/Figures/Glaucus/glaucus.html 
-  # https://en.wikipedia.org/wiki/Glaucus 
+  header=dashboardHeader(title = "YFT review 2022"),
   sidebar=dashboardSidebar(
     br(),
-    #img(src = "spc.png", height = 60),
     br(),
     sidebarMenu(id="sidebarmenu",
       # Add any required menus submenu inside menuItem
