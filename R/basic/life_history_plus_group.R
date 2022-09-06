1c24246616cbf67e920502fbbfa341197f4f92c5
Arni Magnusson <thisisarni@gmail.com>
2022-09-07 03:32:21 +1100

    Extend plus_group.R to life_history_plus_group.R

--- R/basic/life_history_plus_group.R
+++ R/basic/life_history_plus_group.R
@@ -5,11 +5,13 @@ library(TAF)
 # Read
 Diag17 <- read.MFCLRep("z:/yft/2017/assessment/Model_Runs/For_Web/plot-14.par.rep")
 Diag20 <- read.MFCLRep("z:/yft/2020/assessment/ModelRuns/Diagnostic/plot-14.par.rep")
+par17 <- read.MFCLPar("z:/yft/2017/assessment/Model_Runs/For_Web/14.par")
+par20 <- read.MFCLPar("z:/yft/2020/assessment/ModelRuns/Diagnostic/14.par")
 
+# Calculate N at age
 n17 <- apply(popN(Diag17), c("age","year","area"), mean)  # average over seasons
 n17 <- as.matrix(ftable(age~year, data=n17))              # sum over regions
 p17 <- proportions(n17, 1)
-
 n20 <- apply(popN(Diag20), c("age","year","area"), mean)  # average over seasons
 n20 <- as.matrix(ftable(age~year, data=n20))              # sum over regions
 p20 <- proportions(n20, 1)
@@ -21,3 +23,9 @@ barplot(colMeans(p20), main="Average N@A in Diag20")
 
 # Plot growth
 plot.growth(list(Diag20=Diag20, Diag17=Diag17))
+
+# Plot M
+plot.nat.mort(list(Diag20=Diag20, Diag17=Diag17))
+
+# Plot maturity
+plot.maturity(list(Diag20=par20, Diag17=par17))
