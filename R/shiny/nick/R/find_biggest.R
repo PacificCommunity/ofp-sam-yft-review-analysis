d585f85c51b89547725f1a40d84581863614d2b3
Arni Magnusson <thisisarni@gmail.com>
2022-09-12 13:29:09 +1100

    Update find_biggest.R to print biggest filename

--- R/shiny/nick/R/find_biggest.R
+++ R/shiny/nick/R/find_biggest.R
@@ -1,17 +1,19 @@
 find_biggest_par <- function(folder, pattern="^[0-9][0-9]\\.par$", full=TRUE,
                              quiet=FALSE)
 {
-  parfiles <- dir(folder, pattern=pattern, full=full)
+  parfile <- dir(folder, pattern=pattern, full=full)
+  parfile <- max(parfile)
   if(!quiet)
-    print(basename(parfiles))
-  max(parfiles)
+    print(basename(parfile))
+  parfile
 }
 
 find_biggest_rep <- function(folder, pattern="^plot-[0-9][0-9]\\.par.rep$",
                              full=TRUE, quiet=FALSE)
 {
-  parfiles <- dir(folder, pattern=pattern, full=full)
+  parfile <- dir(folder, pattern=pattern, full=full)
+  parfile <- max(parfile)
   if(!quiet)
-    print(basename(parfiles))
-  max(parfiles)
+    print(basename(parfile))
+  parfile
 }
