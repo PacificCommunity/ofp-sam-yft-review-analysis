5b1e1ed09d18f9fbb5e201977efff44b2a860b79
Arni Magnusson <thisisarni@gmail.com>
2022-09-12 16:15:13 +1100

    Plot biomass as lines

--- R/regions/diagnostic_to_4_regions.R
+++ R/regions/diagnostic_to_4_regions.R
@@ -5,14 +5,15 @@ library(TAF)
 Diag20 <- read.MFCLRep(file.path("z:/yft/2020_review/analysis/stepwise",
                                  "17_Diag20/plot-14.par.rep"))
 
-# Extract depletion, biomass, unfished
-cols <- c("year", "area", "data")
-biomass   <- as.data.frame(SB(Diag20,     combine_areas=FALSE))[cols]
-unfished  <- as.data.frame(SBF0(Diag20,   combine_areas=FALSE))[cols]
+# Extract spawning biomass and unfished biomass
+columns <- c("year", "area", "data")
+biomass   <- as.data.frame(SB(Diag20, combine_areas=FALSE))[columns]
+unfished  <- as.data.frame(SBF0(Diag20, combine_areas=FALSE))[columns]
 
 # Store year and area as integers
 biomass <- type.convert(biomass, as.is=TRUE)
 unfished <- type.convert(unfished, as.is=TRUE)
+year <- sort(unique(biomass$year))
 
 # Combine regions
 addDomain <- function(x)
@@ -24,7 +25,6 @@ addDomain <- function(x)
   x$domain[x$area %in% c(5,6,9)] <- "South"
   x
 }
-
 biomass <- aggregate(data~year+domain, addDomain(biomass), sum)
 unfished <- aggregate(data~year+domain, addDomain(unfished), sum)
 
@@ -33,25 +33,18 @@ depletion <- biomass
 depletion$data <- biomass$data / unfished$data
 
 # Plot
-years <- sort(unique(depletion$year))
-
+colors <- palette()[c(2,7,4,5)]
+lwd <- 2
 mkdir("pdf")
-pdf("pdf/diagnostic_to_4_regions.pdf", width=12, height=12)
-par(mfrow=c(2,3))
-barplot(data/1e6~domain+year, biomass, col=2:5, legend=TRUE,
+pdf("pdf/diagnostic_to_4_regions.pdf", width=10, height=5)
+par(mfrow=c(1,3))
+matplot(year, xtabs(data~year+domain, biomass)/1e6,
+        type="l", col=colors, lty=1, lwd=lwd, ylim=lim(biomass$dat/1e6, 1.05),
         xlab="Year", ylab="Spawning biomass (million t)")
-barplot(data/1e6~domain+year, unfished, col=2:5, legend=TRUE,
+matplot(year, xtabs(data~year+domain, unfished)/1e6, type="l",
+        col=colors, lty=1, lwd=lwd, ylim=lim(unfished$dat/1e6, 1.05),
         xlab="Year", ylab="Unfished biomass (million t)")
-matplot(years, xtabs(data~year+domain, depletion), type="l", col=2:5,
-        lty=1, lwd=2, ylim=0:1, xlab="Year", ylab="SB / SBF=0")
-
-matplot(years, xtabs(data~year+domain, biomass)/1e6, type="l", col=2:5, lty=1,
-        lwd=2, ylim=lim(biomass$dat/1e6, 1.05), xlab="Year",
-        ylab="Spawning biomass (million t)")
-matplot(years, xtabs(data~year+domain, unfished)/1e6, type="l", col=2:5, lty=1,
-        lwd=2, ylim=lim(unfished$dat/1e6, 1.05), xlab="Year",
-        ylab="Unfished biomass (million t)")
-matplot(years, xtabs(data~year+domain, depletion), type="l", col=2:5, lty=1,
-        lwd=2, ylim=0:1, xlab="Year", ylab="SB / SBF=0")
+matplot(year, xtabs(data~year+domain, depletion),
+        type="l", col=colors, lty=1, lwd=lwd, ylim=0:1,
+        xlab="Year", ylab="SB / SBF=0")
 dev.off()
-
