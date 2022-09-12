c1b7f51676c46030c2790aaa71aa55aa22b2d0ce
Arni Magnusson <thisisarni@gmail.com>
2022-09-12 12:33:25 +1100

    Development state of nick Shiny app

--- R/shiny/nick/R/find_biggest.R
+++ R/shiny/nick/R/find_biggest.R
@@ -1,12 +1,17 @@
-find_biggest_par <- function(folder, pattern="^[0-9][0-9]\\.par$", full=TRUE)
+find_biggest_par <- function(folder, pattern="^[0-9][0-9]\\.par$", full=TRUE,
+                             quiet=FALSE)
 {
   parfiles <- dir(folder, pattern=pattern, full=full)
+  if(!quiet)
+    print(basename(parfiles))
   max(parfiles)
 }
 
 find_biggest_rep <- function(folder, pattern="^plot-[0-9][0-9]\\.par.rep$",
-                             full=TRUE)
+                             full=TRUE, quiet=FALSE)
 {
   parfiles <- dir(folder, pattern=pattern, full=full)
+  if(!quiet)
+    print(basename(parfiles))
   max(parfiles)
 }
