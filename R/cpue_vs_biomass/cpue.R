5f9b1c8da7a6a22cf70f9cc66f121fde538072f0
Arni Magnusson <thisisarni@gmail.com>
2022-09-13 13:54:03 +1100

    Calculate CPUE by region from Shiny dataset

--- R/cpue_vs_biomass/cpue.R
+++ R/cpue_vs_biomass/cpue.R
@@ -1,3 +1,12 @@
 load("../shiny/stepwise-2020/app/data/other_data.Rdata")
 
-head(cpue_dat)
+cpue <- type.convert(cpue_dat, as.is=TRUE)
+cpue$area <- cpue$fishery - 32L
+cpue <- cpue[cpue$model == "17_Diag20",]
+# 2412 rows = 67 years x 4 seasons x 9 areas
+
+obs <- aggregate(cpue_obs~year+area, cpue, mean)  # average over seasons
+
+obs <- aggregate(cpue_obs~area, cpue, mean)  # average over years
+
+round(obs, 1)
