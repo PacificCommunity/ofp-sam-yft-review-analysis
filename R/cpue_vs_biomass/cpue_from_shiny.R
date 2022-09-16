c20375a8395fdfe1557a4640982bc759ecdd56e2
Arni Magnusson <thisisarni@gmail.com>
2022-09-16 15:26:34 +1100

    Rename cpue_obs -> obs

--- R/cpue_vs_biomass/cpue_from_shiny.R
+++ R/cpue_vs_biomass/cpue_from_shiny.R
@@ -5,16 +5,17 @@ geomean <- function(x) exp(mean(log(x)))
 cpue <- type.convert(cpue_dat, as.is=TRUE)
 cpue$area <- cpue$fishery - 32L
 cpue <- cpue[cpue$model == "17_Diag20",]
+cpue$obs <- cpue$cpue_obs
 ## 2412 rows = 67 years x 4 seasons x 9 areas
 
 ## Average over years and seasons
-mean_obs <- aggregate(cpue_obs~area, cpue, mean)
+mean_obs <- aggregate(obs~area, cpue, mean)
 names(mean_obs)[2] <- "mean_obs"
 
-geomean_obs <- aggregate(cpue_obs~area, cpue, geomean)
+geomean_obs <- aggregate(obs~area, cpue, geomean)
 names(geomean_obs)[2] <- "geomean_obs"
 
-mean_log_obs <- aggregate(log(cpue_obs)~area, cpue, mean)
+mean_log_obs <- aggregate(log(obs)~area, cpue, mean)
 names(mean_log_obs)[2] <- "mean_log_obs"
 
 shiny <- data.frame(mean_obs, geomean_obs[2], mean_log_obs[2])
