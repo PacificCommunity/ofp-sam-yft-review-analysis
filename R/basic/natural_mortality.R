aa904dd8679b6d041d24f7d14b48b409f0175c15
Arni Magnusson <thisisarni@gmail.com>
2022-09-16 14:46:35 +1100

    M coefficient is the geometric mean

--- R/basic/natural_mortality.R
+++ R/basic/natural_mortality.R
@@ -1,5 +1,7 @@
 library(FLR4MFCL)
 
+geomean <- function(x) exp(mean(log(x)))
+
 folder <- "//penguin/assessments/yft/2020_review/analysis/review_runs/arni_john"
 
 rep.diag <- read.MFCLRep(file.path(folder, "ZZ_Diag20/plot-14.par.rep"))
@@ -11,18 +13,8 @@ abline(h=seq(0.1,0.5,0.1), col="lightgray")
 lines(m_at_age(rep.diag), lwd=2)
 lines(m_at_age(rep.mest), lwd=2, col="red")
 
-mean(m_at_age(rep.diag))        # 0.237
-mean(m_at_age(rep.mest))        # 0.207
-
-mean(m_at_age(rep.diag)[6:40])  # 0.217
-mean(m_at_age(rep.mest)[6:40])  # 0.189
-
-mean(m_at_age(rep.mest)[1:40])  # 0.207
-mean(m_at_age(rep.mest)[2:40])  # 0.201
-mean(m_at_age(rep.mest)[3:40])  # 0.196
-mean(m_at_age(rep.mest)[4:40])  # 0.193
-mean(m_at_age(rep.mest)[5:40])  # 0.190
-mean(m_at_age(rep.mest)[6:40])  # 0.189
+geomean(m_at_age(rep.diag))  # 0.232
+geomean(m_at_age(rep.mest))  # 0.202
 
 M_vector <- data.frame(Age=1:40,
                        Diag=m_at_age(rep.diag), Mest=m_at_age(rep.mest))
@@ -43,3 +35,5 @@ write.csv(M_coefficient, "M_coefficient.csv", row.names=FALSE, quote=FALSE)
 
 f <- flags(par.mest)
 f[f$flagtype==2 & f$flag %in% c(5, 33, 73, 77:80, 82:84, 109, 121, 130:133), ]
+
+flagDiff(par.diag, par.mest)
