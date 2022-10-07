58159541219777f53ba4e7e0e1ce740c68324267
Arni Magnusson <thisisarni@gmail.com>
2022-10-07 14:51:24 +1100

    Rename sb -> biomass

--- R/cpue_vs_biomass/data.R
+++ R/cpue_vs_biomass/data.R
@@ -1,7 +1,7 @@
 ## Preprocess data, write TAF data tables
 
 ## Before: plot-14.par.rep, yft.frq (bootstrap/data)
-## After:  cpue.csv, sb.csv (data)
+## After:  cpue.csv, biomass.csv (data)
 
 library(TAF)
 suppressMessages(library(FLR4MFCL))
@@ -12,24 +12,24 @@ mkdir("data")
 frq <- read.MFCLFrq("bootstrap/data/yft.frq")
 rep <- read.MFCLRep("bootstrap/data/plot-14.par.rep")
 
+## Calculate spawning biomass
+biomass <- adultBiomass(rep)
+biomass <- as.data.frame(biomass)
+biomass <- biomass[c("year", "season", "area", "data")]
+names(biomass) <- c("year", "season", "area", "biomass")
+## 2412 rows = 67 years x 4 seasons x 9 areas
+
 ## Calculate CPUE
 cpue <- realisations(frq)  # remove size and length frequencies
 cpue <- cpue[cpue$fishery %in% 33:41,]
 cpue$season <- (cpue$month + 1) / 3
 cpue$area <- cpue$fishery - 32
-cpue$index <- cpue$catch / cpue$effort
-cpue$index[cpue$index < 0] <- NA  # replace -1 with NA
-cpue <- cpue[c("year", "season", "area", "index")]
+cpue$cpue <- cpue$catch / cpue$effort
+cpue$cpue[cpue$cpue < 0] <- NA  # replace -1 with NA
+cpue <- cpue[c("year", "season", "area", "cpue")]
 rownames(cpue) <- NULL
 ## 2412 rows = 67 years x 4 seasons x 9 areas
 
-## Calculate spawning biomass
-sb <- adultBiomass(rep)
-sb <- as.data.frame(sb)
-sb <- sb[c("year", "season", "area", "data")]
-names(sb) <- c("year", "season", "area", "biomass")
-## 2412 rows = 67 years x 4 seasons x 9 areas
-
 ## Write TAF tables
+write.taf(biomass, dir="data")
 write.taf(cpue, dir="data")
-write.taf(sb, dir="data")
