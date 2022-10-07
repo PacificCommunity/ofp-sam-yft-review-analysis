30865437b0f1f1419c3786c1262884359b3b6b69
Arni Magnusson <thisisarni@gmail.com>
2022-10-07 14:19:57 +1100

    Add biomass data

--- R/cpue_vs_biomass/data.R
+++ R/cpue_vs_biomass/data.R
@@ -1,23 +1,35 @@
 ## Preprocess data, write TAF data tables
 
-## Before: yft.frq (bootstrap/data)
-## After:  cpue.csv (data)
+## Before: plot-14.par.rep, yft.frq (bootstrap/data)
+## After:  cpue.csv, sb.csv (data)
 
 library(TAF)
-library(FLR4MFCL)
+suppressMessages(library(FLR4MFCL))
 
 mkdir("data")
 
-## Read frq file
+## Read MFCL files
 frq <- read.MFCLFrq("bootstrap/data/yft.frq")
+rep <- read.MFCLRep("bootstrap/data/plot-14.par.rep")
 
 ## Calculate CPUE
 cpue <- realisations(frq)  # remove size and length frequencies
 cpue <- cpue[cpue$fishery %in% 33:41,]
+cpue$season <- (cpue$month + 1) / 3
 cpue$area <- cpue$fishery - 32
-cpue$obs <- cpue$catch / cpue$effort
-cpue$obs[cpue$obs < 0] <- NA  # replace -1 with NA
+cpue$index <- cpue$catch / cpue$effort
+cpue$index[cpue$index < 0] <- NA  # replace -1 with NA
+cpue <- cpue[c("year", "season", "area", "index")]
+rownames(cpue) <- NULL
 ## 2412 rows = 67 years x 4 seasons x 9 areas
 
-## Write CPUE data
+## Calculate spawning biomass
+sb <- adultBiomass(rep)
+sb <- as.data.frame(sb)
+sb <- sb[c("year", "season", "area", "data")]
+names(sb) <- c("year", "season", "area", "biomass")
+## 2412 rows = 67 years x 4 seasons x 9 areas
+
+## Write TAF tables
 write.taf(cpue, dir="data")
+write.taf(sb, dir="data")
