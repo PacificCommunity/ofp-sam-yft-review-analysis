0d24ae0588e581ea5d29acb3124aa5a79a7865c2
Arni Magnusson <thisisarni@gmail.com>
2022-11-21 12:53:38 +1100

    Rename original model.R to model_cpue_bio.R

--- R/cpue_vs_biomass/model.R
+++ R/cpue_vs_biomass/model.R
@@ -1,24 +1,6 @@
 ## Run analysis, write model results
 
-## Before: biomass.csv, cpue.csv (data)
-## After:  prop.csv (model)
-
 library(TAF)
 
-mkdir("model")
-
-## Read data
-biomass <- read.taf("data/biomass.csv")
-cpue <- read.taf("data/cpue.csv")
-
-## Calculate average by area
-cpue <- aggregate(cpue~area, cpue, mean)
-biomass <- aggregate(biomass~area, biomass, mean)
-
-## Proportion of total
-prop <- merge(cpue, biomass)
-prop$cpue <- prop.table(prop$cpue)
-prop$biomass <- prop.table(prop$biomass)
-
-## Write results
-write.taf(prop, dir="model")
+sourceTAF("model_cpue_bio")
+sourceTAF("model_cpue_cor")
