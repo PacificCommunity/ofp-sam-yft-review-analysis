15c9e105c2fb5cd019488f2c1e5c12ebcd459886
Arni Magnusson <thisisarni@gmail.com>
2022-11-21 13:11:59 +1100

    Copy CPUE tables to output folder

--- R/cpue_vs_biomass/output.R
+++ R/cpue_vs_biomass/output.R
@@ -1,10 +1,13 @@
 ## Extract results of interest, write TAF output tables
 
-## Before: prop.csv (model)
-## After:  prop.csv (output)
+## Before: cpue_avg.csv, cpue_cor.csv, cpue_cor_matrix.csv, prop.csv (model)
+## After:  cpue_avg.csv, cpue_cor.csv, cpue_cor_matrix.csv, prop.csv (output)
 
 library(TAF)
 
 mkdir("output")
 
+cp("model/cpue_avg.csv", "output")
+cp("model/cpue_cor.csv", "output")
+cp("model/cpue_cor_matrix.csv", "output")
 cp("model/prop.csv", "output")
